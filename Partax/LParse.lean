67aae88f6f9dca687de2eeca5cc9764dde213f9d
tydeu <tydeu@hatpress.net>
2023-07-29 23:54:05 -0400

    chore: fix grammar in copyright comment

--- Partax/LParse.lean
+++ Partax/LParse.lean
@@ -1,6 +1,6 @@
 /-
 Copyright (c) 2022-2023 Mac Malone. All rights reserved.
-Released under Apache 2.0 license as described in the file LICENSE.
+Released under the Apache 2.0 license as described in the file LICENSE.
 Authors: Mac Malone
 -/
 import Lean.Syntax
